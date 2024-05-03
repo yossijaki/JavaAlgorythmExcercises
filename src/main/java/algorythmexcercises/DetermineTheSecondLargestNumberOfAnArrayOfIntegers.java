968d7d54baea8b318a7bb542087e13137c4fc343
Yossijaki <yossijaki@gmail.com>
2024-05-03 09:44:37 -0600

    Updating the reverse polish notation class and adding a List processor class

---
+++ src/main/java/algorythmexcercises/DetermineTheSecondLargestNumberOfAnArrayOfIntegers.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.algorythmexcercises;
+/**
+ *
+ * @author Orochi
+ */
+public class DetermineTheSecondLargestNumberOfAnArrayOfIntegers {
+    
+    static int secondLargest(int[] numbers) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int number : numbers) {
+            if (number > max) {
+                secondMax = max;
+                max = number;
+            } else if (number > secondMax && number < max) {
+                secondMax = number;
+            }
+        }
+
+        if (secondMax == Integer.MIN_VALUE) {
+            throw new RuntimeException("No second largest number found");
+        } else {
+            return secondMax;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(secondLargest(numbers)); // Outputs: 4
+        
+        
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        
+        for (int number : numbers) {
+            if (number > max) {
+                secondMax = max;
+                max = number;
+            } else if (number > secondMax && number < max) {
+                secondMax = number;
+            }
+        }
+    }
+}
