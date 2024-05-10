ab1f10ffed358f3f2f90e92ea7ab66861facfcc2
Yossijaki <yossijaki@gmail.com>
2024-05-09 19:35:50 -0600

    addin exercise statement

--- src/main/java/algorythmexcercises/VisitCounter.java
+++ src/main/java/algorythmexcercises/VisitCounter.java
@@ -4,6 +4,25 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/*
+The system you work with on a daily basis runs multiple microservices.
+You have been asked to prepare an aggregation that represents the number of user visits to all microservices, for use by data analysts.
+The architect you're working with has already designed the API, which consists of a single class: 
+VisitCounter. VisitCounter has a single method, count.
+It returns Map<Long, Long>  - this map should contain the number of visits by the user with a given ID.
+This method accepts an array of Map<String, UserStats>. Every map represents the total number of visits per user to a given microservice.
+There are some problems, however:
+    • The map key, which is a String, should be parseable to Long – but it not be. You must skip any such faulty entries.
+    • For some keys, UserStats may be null. You must skip any such faulty entries.
+    • UserStats has a single field, visitCount, of type Optional<Long>. A getter for this field is also implemented. This field will never be null; however, it might be empty. You must skip any such faulty entries.
+
+Remember that you may receive some invalid input:  null, empty maps, and so on. Handle it all appropriately and return an empty map.
+Assumptions
+    • The UserStats class is already implemented. You do not have to write the implementation of this class as a part of your solution.
+Available packages and libraries
+    • You may use Java 8 Streams API to implement your solution.
+*/
+
 class VisitCounter {
 
     Map<Long, Long> count(Map<String, UserStats>... visits) {
@@ -39,34 +58,3 @@ class VisitCounter {
         }
     }
 }
-
-/* The first version
-class VisitCounter1 {
-
-    Map<Long, Long> count(Map<String, UserStats>... visits) {
-        // Create a stream of all maps
-        Stream<Map<String, UserStats>> mapStream = Arrays.stream(visits);
-
-        // Filter out faulty entries (null maps, non-parseable keys, and empty UserStats)
-        Map<Long, Long> result = mapStream
-                .filter(Objects::nonNull) // Skip null maps
-                .flatMap(map -> map.entrySet().stream())
-                .filter(entry -> isValidKey(entry.getKey()))
-                .filter(entry -> entry.getValue() != null && entry.getValue().getVisitCount().isPresent())
-                .collect(Collectors.groupingBy(
-                        entry -> Long.parseLong(entry.getKey()),
-                        Collectors.summingLong(entry -> entry.getValue().getVisitCount().orElse(0L))
-                ));
-
-        return result;
-    }
-
-    private boolean isValidKey(String key) {
-        try {
-            Long.parseLong(key);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-}*/
